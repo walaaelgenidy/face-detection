114108af3187661f62b4d10ef46740c039411d1b
walaaelgenidy <walaaelgenidy@gitlab.com>
2020-03-16 04:46:31 +0200

    first

--- src/index.js
+++ src/index.js
@@ -1,5 +1,7 @@
 import React from 'react';
 import ReactDOM from 'react-dom';
+import 'tachyons';
+import 'react-tilt';
 import './index.css';
 import App from './App';
 import * as serviceWorker from './serviceWorker';
