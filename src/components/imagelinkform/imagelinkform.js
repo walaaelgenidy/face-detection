3753d7412467429457118ab42c0235ea045528a0
walaaelgenidy <walaaelgenidy@gitlab.com>
2020-03-27 09:43:34 +0200

    Update imagelinkform.js

--- src/components/imagelinkform/imagelinkform.js
+++ src/components/imagelinkform/imagelinkform.js
@@ -11,7 +11,7 @@ const Imagelinkform = ()=>{
           <div className='form center pa4 br3 shadow-5'>
             <input className='f4 pa2 w-70 center' type='tex' />
             <button
-              className='w-30 grow f4 link ph3 pv2 dib white bg-light-blue'
+              className='w-30 grow f4 link ph3 pv2 dib white bg-blue'
               
             >Detect</button>
           </div>
