a317c5e924495f7610f00bb4ce6f9dc1aa3dc10c
walaaelgenidy <45464802+walaaelgenidy@users.noreply.github.com>
2020-05-20 00:42:23 +0200

    Updates

---
+++ precache-manifest.d6872c877286c40f87e4f42f38ed6d3e.js
@@ -0,0 +1,30 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "090133814495deb017841a1d9a82ce4f",
+    "url": "/face-detection/index.html"
+  },
+  {
+    "revision": "6045c39a6299f155c571",
+    "url": "/face-detection/static/css/2.3c03f26f.chunk.css"
+  },
+  {
+    "revision": "2337f84c785f998984d5",
+    "url": "/face-detection/static/css/main.33d56b83.chunk.css"
+  },
+  {
+    "revision": "6045c39a6299f155c571",
+    "url": "/face-detection/static/js/2.89ee988d.chunk.js"
+  },
+  {
+    "revision": "e88a3e95b5364d46e95b35ae8c0dc27d",
+    "url": "/face-detection/static/js/2.89ee988d.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "2337f84c785f998984d5",
+    "url": "/face-detection/static/js/main.d63ee831.chunk.js"
+  },
+  {
+    "revision": "a08dae4214096b7c7e44",
+    "url": "/face-detection/static/js/runtime-main.2ea25f14.js"
+  }
+]);
