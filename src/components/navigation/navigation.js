041f694bdf0ec6658859992e936d8786770f90b0
walaaelgenidy <45464802+walaaelgenidy@users.noreply.github.com>
2020-05-19 23:14:20 +0200

    Revert "signin and register components"

--- src/components/navigation/navigation.js
+++ src/components/navigation/navigation.js
@@ -1,22 +1,11 @@
 import React from 'react';
 
-const Navigation = ({onRouteChange , isSignedIn}) => {
-    if(isSignedIn){
+const Navigation = () => {
     return(
         <nav style={{display: 'flex', justifyContent:'flex-end'}}>
-            <p onClick={()=>onRouteChange('signout')} className='f3 link dim black underline pa3 pointer '>sign out</p>
+            <p className='f3 link dim black underline pa3 pointer '>sign out</p>
         </nav>
     );
-    }
-    else{
-        return(
-            <nav style={{display: 'flex', justifyContent:'flex-end'}}>
-                <p onClick={()=>onRouteChange('signin')} className='f3 link dim black underline pa3 pointer '>sign in</p>
-                <p onClick={()=>onRouteChange('register')} className='f3 link dim black underline pa3 pointer '>Register</p>
-               </nav>
-            );
-    }  
-
 }
 
 export default Navigation ; 
